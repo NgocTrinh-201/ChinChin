35eba710252c0158ed3da30cc369145de43c929d
NgocTrinh-201 <trinhdo112001@gmail.com>
2023-05-09 06:40:30 -0700

    Initial Commit

---
+++ ChinChin/ChinChinApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChinChinApp.swift
+//  ChinChin
+//
+//  Created by Do Trinh on 09/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct ChinChinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChinChin/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChinChin
+//
+//  Created by Do Trinh on 09/05/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
